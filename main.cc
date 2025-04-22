0c4d4cefde0fe98377f4c0c0559c7bcd3fb732d0
Ali hassan <alihassan2023@protonmail.com>
2025-04-23 00:56:52 +0500

    Basic file structure of cpp added

---
+++ main.cc
@@ -0,0 +1,56 @@
+#include "crow.h"
+//#include "crow_all.h"
+
+int main()
+{
+    crow::SimpleApp app; //define your crow application
+
+    CROW_ROUTE(app, "/")([](){
+        return "Hello world";
+    });
+
+    CROW_ROUTE(app, "/inbox")([](){
+        auto page = crow::mustache::load_text("inbox.html");
+        return page;
+    });
+
+    CROW_ROUTE(app, "/greet/<string>")([](std::string name){ // 
+        auto page = crow::mustache::load("fancypage.html"); // 
+        crow::mustache::context ctx ({{"person", name}}); // 
+        return page.render(ctx); //
+    });
+
+    CROW_ROUTE(app,"/add/<int>/<int>")
+    ([](int int1,int int2){
+        return std::to_string(int1+int2);
+    });
+
+    CROW_ROUTE(app,"/subtract/<int>/<int>")
+    ([](int int1,int int2){
+        return std::to_string(int1-int2);
+    });
+    CROW_ROUTE(app,"/testjson")
+    (
+        [](){
+            crow::json::wvalue test;
+            test["hi"] = "string";
+            return test;
+        }
+    );
+
+    CROW_WEBSOCKET_ROUTE(app, "/ws")
+    .onopen([&](crow::websocket::connection& conn){
+            std::cout << "Opened the connection" << std::endl;
+            })
+    .onclose([&](crow::websocket::connection& conn, const std::string& reason, uint16_t){
+        std::cout << "Closed the connection" << std::endl;
+            
+            })
+    .onmessage([&](crow::websocket::connection& /*conn*/, const std::string& data, bool is_binary){
+            std::cout << "Got the message" << std::endl;
+            std::cout << data << std::endl;
+
+            });
+    //set the port, set the app to run on multiple threads, and run the app
+    app.port(18080).multithreaded().run();
+}
---
+++ static/inbox.js
@@ -0,0 +1,12 @@
+const button = document.querySelector('#send');
+const input = document.querySelector('#message');
+const wb = new WebSocket('ws://localhost:18080/ws');
+wb.addEventListener('open', () => {
+    console.log('WebSocket connection opened');
+});
+button.addEventListener('click', () => {
+    jsonSend = {"message": input.value};
+    wb.send(JSON.stringify(jsonSend));
+    // wb.send(input.value);
+    console.log('Message sent to server');
+})
